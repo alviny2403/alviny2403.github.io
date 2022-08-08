a18aa9e7886b37fd3f4482738da7a50b647b37e4
alviny2403 <alvinyan12345@gmail.com>
2022-08-08 00:17:07 -0400

    added js stuff

---
+++ data.js
@@ -0,0 +1,13 @@
+let aboutMe= {
+    "name": {
+        "first": "Alvin",
+        "last": "Yan"
+    },
+    "age": 17,
+    "year": "senior",
+    "date": {
+        "year": "2022",
+        "month": "August",
+    },
+    "major": "mechanical engineering / computer science"
+}
---
+++ script.js
@@ -0,0 +1,27 @@
+let editText= document.getElementById("textEdit")
+
+let enterBtn= document.getElementById("enter")
+enterBtn.onclick= (event)=>{
+    event.preventDefault()
+
+    let newText= document.getElementById("textEntered")
+    let newTextValue= newText.value
+    
+    if (newTextValue == ""){
+        editText.innerHTML= "(hello world)"
+        editText.style.color= "grey"
+    }
+    else {
+        editText.innerHTML= newTextValue
+        editText.style.color= "black"
+    }
+}
+
+// stuff about me that is subject to change
+let myData= document.getElementById("myData")
+myData.innerHTML= "High school "+ 
+    aboutMe.year+ " and aspiring "+ 
+    aboutMe.major+ " major. I am curretly "+ 
+    aboutMe.age+ " years old (as of "+
+    aboutMe.date.month+ " "+ 
+    aboutMe.date.year+ ")."
